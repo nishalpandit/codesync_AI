b26521deaeda88c74afdd6a56824c8ee7d5fdc39
Nishal Kumar pandit <136226157+nishalpandit@users.noreply.github.com>
2025-12-04 12:15:04 +0530

    Update authentication to use email instead of username

--- Codesync/custom_backed.py
+++ Codesync/custom_backed.py
@@ -1,16 +1,19 @@
-from .models import User
+from django.contrib.auth.models import User
 from django.contrib.auth.backends import BaseBackend
 
 
 class CustomBackend(BaseBackend):
-    def authenticate(self, request, username, password, **kwargs):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        email = username or kwargs.get('email')
+        if email is None or password is None:
+            return None
         try:
-            user = User.objects.get(email=username)
+            user = User.objects.get(email=email)
             if user.check_password(password):
                 return user
         except User.DoesNotExist:
             return None
-        
+    
     def get_user(self, user_id):
         try:
             return User.objects.get(pk=user_id)
